7bd19045024abe967b4ae466465ae5b296a91348
tmdrkdtn1-project <tmdrkdtn1@gmail.com>
2026-09-20 10:19:27 +0900

    Bump prototype to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.luckygirls.lastwall"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.0"
+        versionCode = 16
+        versionName = "1.4.0"
     }
 
     compileOptions {
