42e72c2aa9b97a1b4c8ba75faf30749dc5489328
tmdrkdtn1-project <tmdrkdtn1@gmail.com>
2026-09-20 09:29:18 +0900

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application") }
+android {
+ namespace = "com.luckygirls.lastwall"
+ compileSdk = 35
+ defaultConfig { applicationId = "com.luckygirls.lastwall"; minSdk = 26; targetSdk = 35; versionCode = 5; versionName = "0.5.0" }
+}
