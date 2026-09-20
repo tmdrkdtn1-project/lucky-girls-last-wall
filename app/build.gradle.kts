46c6ab392b7bd28a2ee91edfbe0b7ea89f5d0f6b
tmdrkdtn1-project <tmdrkdtn1@gmail.com>
2026-09-20 09:44:14 +0900

    Compile MainActivity and bump to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,15 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 android {
- namespace = "com.luckygirls.lastwall"
- compileSdk = 35
- defaultConfig { applicationId = "com.luckygirls.lastwall"; minSdk = 26; targetSdk = 35; versionCode = 5; versionName = "0.5.0" }
+    namespace = "com.luckygirls.lastwall"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.luckygirls.lastwall"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 6
+        versionName = "0.5.1"
+    }
 }
