e400e2db6bfbefae1558a3dab64ebbd4c76812e4
tmdrkdtn1-project <tmdrkdtn1@gmail.com>
2026-09-20 18:20:11 +0900

    fix: recover WebView if renderer process exits

--- app/src/main/java/com/luckygirls/lastwall/MainActivity.kt
+++ app/src/main/java/com/luckygirls/lastwall/MainActivity.kt
@@ -3,12 +3,24 @@ import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.RenderProcessGoneDetail
 class MainActivity : Activity() {
  private lateinit var webView: WebView
  override fun onCreate(savedInstanceState: Bundle?) {
   super.onCreate(savedInstanceState)
   webView=WebView(this)
-  webView.webViewClient=WebViewClient()
+  webView.webViewClient=object: WebViewClient() {
+   override fun onRenderProcessGone(view: WebView?, detail: RenderProcessGoneDetail?): Boolean {
+    try { view?.destroy() } catch (_: Exception) {}
+    webView=WebView(this@MainActivity)
+    webView.webViewClient=this
+    webView.settings.javaScriptEnabled=true
+    webView.settings.domStorageEnabled=true
+    webView.loadUrl("file:///android_asset/index.html")
+    setContentView(webView)
+    return true
+   }
+  }
   webView.settings.javaScriptEnabled=true
   webView.settings.domStorageEnabled=true
   webView.loadUrl("file:///android_asset/index.html")
