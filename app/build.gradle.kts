918d62cccbd0b6dc1629e3c69407214fb4c6ba60
tmdrkdtn1-project <tmdrkdtn1@gmail.com>
2026-09-20 15:59:32 +0900

    chore: bump prototype to 2.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.luckygirls.lastwall"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "2.14.0"
+        versionCode = 38
+        versionName = "2.15.0"
     }
 
     compileOptions {
